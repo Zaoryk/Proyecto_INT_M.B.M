9736199d72959f34842982ae3dc37d52af2a6ddb
marlontabi <129401074+marlontabi@users.noreply.github.com>
2025-09-30 18:16:48 -0300

    actu

--- dulceria/dispositivos/admin.py
+++ dulceria/dispositivos/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Bodega, Cliente, Costo, Listarprecios, Movimientoinventario, Ordendecompra, Ordenproduccion, Pedido, Producto, Proveedor, Usuario
+from .models import Bodega, Cliente, Costo, ListarPrecios, MovimientoInventario, OrdenDeCompra, OrdenProduccion, Pedido, Producto, Proveedor, Usuario
 
 admin.site.register(Bodega)
 admin.site.register(Cliente)
 admin.site.register(Costo)
-admin.site.register(Listarprecios)
-admin.site.register(Movimientoinventario)
-admin.site.register(Ordendecompra)
-admin.site.register(Ordenproduccion)
+admin.site.register(ListarPrecios)
+admin.site.register(MovimientoInventario)
+admin.site.register(OrdenDeCompra)
+admin.site.register(OrdenProduccion)
 admin.site.register(Pedido)
 admin.site.register(Producto)
 admin.site.register(Proveedor)
--- dulceria/dispositivos/models.py
+++ dulceria/dispositivos/models.py
@@ -1,41 +1,24 @@
 from django.db import models
 
 
-class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)
+class Bodega(models.Model):
+    idbodega = models.AutoField(db_column='idBodega', primary_key=True)
+    nombre = models.CharField(max_length=120, blank=True, null=True)
+    ubicacion = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'bodega'
+
+
+class Cliente(models.Model):
+    idcliente = models.AutoField(db_column='idCliente', primary_key=True)
     nombre = models.CharField(max_length=100, blank=True, null=True)
-    rol = models.CharField(max_length=50, blank=True, null=True)
-    permisos = models.CharField(max_length=255, blank=True, null=True)
-    password = models.CharField(max_length=255)
-    email = models.CharField(max_length=120, blank=True, null=True)
+    tipo = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'Usuario'
-
-
-class SolicitudCompra(models.Model):
-    id = models.AutoField(primary_key=True)
-    fecha = models.DateField(blank=True, null=True)
-    estado = models.CharField(max_length=45, blank=True, null=True)
-    monto_total = models.IntegerField(blank=True, null=True)
-    usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_id')
-
-    class Meta:
-        managed = False
-        db_table = 'solicitudCompra'
-
-
-class PedidoDeVenta(models.Model):
-    id_pedido_de_venta = models.AutoField(primary_key=True)
-    fecha = models.DateField(blank=True, null=True)
-    estado = models.CharField(max_length=45, blank=True, null=True)
-    monto_total = models.IntegerField(blank=True, null=True)
-    usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_id')
-
-    class Meta:
-        managed = False
-        db_table = 'Pedido_de_venta'
+        db_table = 'cliente'
 
 
 class Producto(models.Model):
@@ -48,42 +31,57 @@ class Producto(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'Producto'
+        db_table = 'producto'
 
 
-class Receta(models.Model):
-    idreceta = models.AutoField(db_column='idReceta', primary_key=True)
-    version = models.CharField(max_length=20, blank=True, null=True)
-    insumos_necesarios = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'Receta'
-
-
-class OrdenProduccion(models.Model):
-    id = models.AutoField(primary_key=True)
-    fechainicio = models.DateField(db_column='fechaInicio', blank=True, null=True)
-    fechafin = models.DateField(db_column='fechaFin', blank=True, null=True)
-    estado = models.CharField(max_length=45, blank=True, null=True)
-    usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_id')
-    producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='Producto_idProducto')
-    receta = models.ForeignKey('Receta', models.DO_NOTHING, db_column='Receta_idReceta')
+class Costo(models.Model):
+    idcosto = models.AutoField(db_column='idCosto', primary_key=True)
+    tipo = models.CharField(max_length=45, blank=True, null=True)
+    monto = models.IntegerField(blank=True, null=True)
+    producto = models.ForeignKey(Producto, models.DO_NOTHING, db_column='Producto_idProducto')
 
     class Meta:
         managed = False
-        db_table = 'OrdenProduccion'
+        db_table = 'costo'
+        unique_together = (('idcosto', 'producto'),)
+
+
+class ListarPrecios(models.Model):
+    idlistarprecios = models.AutoField(db_column='idListarPrecios', primary_key=True)
+    canal = models.CharField(db_column='Canal', max_length=50, blank=True, null=True)
+    temporada = models.CharField(db_column='Temporada', max_length=45, blank=True, null=True)
+    valor = models.IntegerField(db_column='Valor', blank=True, null=True)
+    cliente = models.ForeignKey(Cliente, models.DO_NOTHING, db_column='Cliente_idCliente')
+
+    class Meta:
+        managed = False
+        db_table = 'listarprecios'
+        unique_together = (('idlistarprecios', 'cliente'),)
+
+
+class MovimientoInventario(models.Model):
+    idmovimiento = models.AutoField(db_column='idMovimientoInventario', primary_key=True)
+    tipo = models.CharField(max_length=45, blank=True, null=True)
+    fecha = models.DateField(blank=True, null=True)
+    cantidad = models.IntegerField(blank=True, null=True)
+    bodega = models.ForeignKey(Bodega, models.DO_NOTHING, db_column='Bodega_idBodega')
+    producto = models.ForeignKey(Producto, models.DO_NOTHING, db_column='Producto_idProducto')
+
+    class Meta:
+        managed = False
+        db_table = 'movimientoinventario'
+        unique_together = (('idmovimiento', 'bodega', 'producto'),)
 
 
 class Proveedor(models.Model):
     id_proveedor = models.AutoField(db_column='id_Proveedor', primary_key=True)
     nombre = models.CharField(max_length=130, blank=True, null=True)
     contacto = models.CharField(max_length=200, blank=True, null=True)
-    condicionescomerciales = models.CharField(db_column='condicionesComerciales', max_length=45, blank=True, null=True)
+    email = models.CharField(max_length=120, blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'Proveedor'
+        db_table = 'proveedor'
 
 
 class OrdenDeCompra(models.Model):
@@ -91,43 +89,50 @@ class OrdenDeCompra(models.Model):
     fecha = models.DateField(blank=True, null=True)
     estado = models.CharField(max_length=45, blank=True, null=True)
     monto_total = models.IntegerField(blank=True, null=True)
-    proveedor = models.ForeignKey('Proveedor', models.DO_NOTHING, db_column='proveedor_id_proveedor')
-    solicitudcompra = models.ForeignKey('SolicitudCompra', models.DO_NOTHING, db_column='solicitudCompra_id')
+    proveedor = models.ForeignKey(Proveedor, models.DO_NOTHING, db_column='proveedor_id_proveedor')
 
     class Meta:
         managed = False
-        db_table = 'ordendeCompra'
+        db_table = 'ordendecompra'
+        unique_together = (('id', 'proveedor'),)
 
 
-class Bodega(models.Model):
-    idbodega = models.AutoField(db_column='idBodega', primary_key=True)
-    nombre = models.CharField(max_length=120, blank=True, null=True)
-    ubicacion = models.CharField(max_length=100, blank=True, null=True)
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    rol = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=120, blank=True, null=True)
+    listarprecios = models.ForeignKey(ListarPrecios, models.DO_NOTHING, db_column='ListarPrecios_idListarPrecios')
 
     class Meta:
         managed = False
-        db_table = 'Bodega'
+        db_table = 'usuario'
 
 
-class MovimientoInventario(models.Model):
-    idmovimientoinventario = models.AutoField(db_column='idMovimientoInventario', primary_key=True)
-    tipo = models.CharField(max_length=45, blank=True, null=True)
+class OrdenProduccion(models.Model):
+    id = models.AutoField(primary_key=True)
+    fechainicio = models.DateField(db_column='fechaInicio', blank=True, null=True)
+    fechafin = models.DateField(db_column='fechaFin', blank=True, null=True)
+    estado = models.CharField(max_length=45, blank=True, null=True)
+    usuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='Usuario_id')
+    producto = models.ForeignKey(Producto, models.DO_NOTHING, db_column='Producto_idProducto')
+
+    class Meta:
+        managed = False
+        db_table = 'ordenproduccion'
+        unique_together = (('id', 'usuario', 'producto'),)
+
+
+class Pedido(models.Model):
+    idpedido = models.AutoField(db_column='idPedido', primary_key=True)
     fecha = models.DateField(blank=True, null=True)
-    cantidad = models.CharField(max_length=45, blank=True, null=True)
-    bodega = models.ForeignKey('Bodega', models.DO_NOTHING, db_column='Bodega_idBodega')
-    producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='Producto_idProducto')
+    monto_total = models.IntegerField(blank=True, null=True)
+    usuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='Usuario_id')
+    cliente = models.ForeignKey(Cliente, models.DO_NOTHING, db_column='Cliente_idCliente')
+    ordendecompra = models.ForeignKey(OrdenDeCompra, models.DO_NOTHING, db_column='OrdendeCompra_id')
 
     class Meta:
         managed = False
-        db_table = 'MovimientoInventario'
-
-
-class Costo(models.Model):
-    idcosto = models.AutoField(db_column='idCosto', primary_key=True)
-    tipo = models.CharField(max_length=45, blank=True, null=True)
-    monto = models.IntegerField(blank=True, null=True)
-    producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='Producto_idProducto')
-
-    class Meta:
-        managed = False
-        db_table = 'Costo'
+        db_table = 'pedido'
+        unique_together = (('idpedido', 'usuario', 'cliente', 'ordendecompra'),)
