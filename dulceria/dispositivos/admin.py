2eb2783a85f63dd85f5d11377ee8de8e2c98e10a
marlontabi <129401074+marlontabi@users.noreply.github.com>
2025-10-03 13:21:51 -0300

    wao polfin

--- dulceria/dispositivos/admin.py
+++ dulceria/dispositivos/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Bodega, Cliente, Costo, ListarPrecios, MovimientoInventario, OrdenDeCompra, OrdenProduccion, Pedido, Producto, Proveedor, Usuario
+from dispositivos.models import Bodega, Cliente, Costo, ListarPrecios, MovimientoInventario, OrdenDeCompra, OrdenProduccion, Pedido, Producto, Proveedor, Usuario
+class ListarPreciosInline(admin.TabularInline):
+    model = ListarPrecios
+    extra = 0
+    fields = ("canal", "temporada", "valor")
+    show_change_link = True
 
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "tipo")
+    inlines = [ListarPreciosInline]
 admin.site.register(Bodega)
-admin.site.register(Cliente)
-admin.site.register(Costo)
 admin.site.register(ListarPrecios)
+admin.site.register(Costo)
 admin.site.register(MovimientoInventario)
 admin.site.register(OrdenDeCompra)
 admin.site.register(OrdenProduccion)
@@ -12,3 +20,4 @@ admin.site.register(Pedido)
 admin.site.register(Producto)
 admin.site.register(Proveedor)
 admin.site.register(Usuario)
+admin.register(ListarPrecios)
